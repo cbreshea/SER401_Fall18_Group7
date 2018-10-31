02f331d266169d56e683b8f0d3febf9a52f15034
Jared LeBlanc <jllebla1@asu.edu>
2018-10-30 19:07:29 -0700

    Task #10 complete
    The Code now compiles and works with the Simple Serial port terminal plugin

--- SampleCodeForSerialTerminalPlugin/main.c
+++ SampleCodeForSerialTerminalPlugin/main.c
@@ -5,26 +5,31 @@
  * Created on October 25, 2018, 8:00 PM
  */
 
-
 #include <avr/io.h>
-#include <util/setbaud.h>
+
+   #define F_CPU 4000000
+
+static void
+uart_9600(void)
+{
+   #define BAUD 9600
+   #include <util/setbaud.h>
+UBRR0H = UBRRH_VALUE;
+UBRR0L = UBRRL_VALUE;
+   #if USE_2X
+   UCSRA |= (1 << U2X);
+   #else
+   UCSR0A &= ~(1 << U2X0);
+   #endif
+   UCSR0B = (1 << TXEN0) | (1 << RXEN0);
+   UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);   /* 8 data bits, 1 stop bit */
+   }
 
 int main(void) {
+    uart_9600();
     char serialChar;
     
     DDRB = 0xff;
-    
-    UBRR0H = UBRRH_VALUE; //defined in setbaud.h
-    UBRR0L = UBRRL_VALUE; //defined in setbaud.h
-  #if USE_2X
-    UCSR0A |= (1 << U2X0);
-  #else
-    UCSR0A &= ~(1 << U2X0);
-  #endif
-                                    /* Enable USART transmitter/receiver */
-    UCSR0B = (1 << TXEN0) | (1 << RXEN0);
-    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);   /* 8 data bits, 1 stop bit */
-    
             
     while (1) {
       loop_until_bit_is_set(UCSR0A, UDRE0);
@@ -41,4 +46,3 @@ void printString(const char myString[]) {
     i++;
   }
 }
-
