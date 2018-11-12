789c56c086db57eee2c83e00827fd29a82993a2c
ksbeasle <ksbeasle@asu.edu>
2018-11-12 00:08:14 -0700

    Stack Management task #31
    progress on stack management and how it works

---
+++ CommonArduinoLibraryExamples/Stack/stack_management/stack_management.ino
@@ -0,0 +1,25 @@
+//Flash memory is where the arduino is sketch is stored
+//SRAM (static random access memory) is where the sketch creates and manipulates variables
+//EEPROM is memory space that programmers use for long term information
+#include <StackArray.h>
+const String msg = "Arduino is cool!";
+
+StackArray<char> stack;
+
+
+
+//Depending on what chip/board you are using you can use up a lot of space very quickly
+
+//This puts 33 bytes into SRAM
+char message[] = "I support the Cape Wind project.";
+
+void setup() {
+  //start communication
+  Serial.begin(9999);
+
+  Serial.println("Here is a string: " + msg);
+
+}
+void loop(){
+  
+}
