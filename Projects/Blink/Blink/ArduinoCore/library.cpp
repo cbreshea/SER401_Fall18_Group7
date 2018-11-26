821dfe9c31aefd74267832071d979712941a0fb6
Jared LeBlanc <jllebla1@asu.edu>
2018-11-26 15:39:18 -0700

    Task#50 complete
    converts the standard example Blink program into a c++ program that works on atmel studio 7 and supports the 328p Xplained-Mini's built in debugging

---
+++ Projects/Blink/Blink/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 11/26/2018 2:18:56 PM
+ * Author : Jared
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
