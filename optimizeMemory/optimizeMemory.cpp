7e508585462585b558343f3b31a1757c5f23e681
Unknown <jllebla1@asu.edu>
2019-01-19 20:49:15 -0700

    Task #82 complete
    chips, like the tiny series and the 168p, were commented out as they are not needed for this program. The test was run and the appropriate 328p was recommended

--- optimizeMemory/optimizeMemory.cpp
+++ optimizeMemory/optimizeMemory.cpp
@@ -5,7 +5,7 @@
 
 int min(int *arr, int num);
 void recommend(int num, int *arr);
-int sketch_memory = 31000;
+int sketch_memory = 300;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
@@ -16,43 +16,43 @@ int main(int argc, const char * argv[]) {
    
 
 	std::ifstream i("MegaAVR.json");
-    std::ifstream a("TinyAVR.json");
+    //std::ifstream a("TinyAVR.json");
     json j = json::parse(i);
-    json b = json::parse(a);
+    //json b = json::parse(a);
 
     // MegaAVRs
-    json ATmega168p = j["ATmega168p"];
+    //json ATmega168p = j["ATmega168p"];
     json ATmega328p = j["ATmega328p"];
     json ATmega644p = j["ATmega644p"];
     json ATmega1284 = j["ATmega1284"];
     json ATmega2560 = j["ATmega2560"];
 
     //TinyAVRS
-    json ATtiny3217 = b["ATtiny3217"];
-    json ATtiny1617 = b["ATtiny1617"];
-    json ATtiny817 = b["ATtiny817"];
+    //json ATtiny3217 = b["ATtiny3217"];
+    //json ATtiny1617 = b["ATtiny1617"];
+    //json ATtiny817 = b["ATtiny817"];
 
     
   
-    int ATmega168p_kb = ATmega168p["Program Memory Size(KB)"];
+    //int ATmega168p_kb = ATmega168p["Program Memory Size(KB)"];
     int ATmega328p_kb = ATmega328p["Program Memory Size(KB)"];
     int ATmega644p_kb = ATmega644p["Program Memory Size(KB)"];
     int ATmega1284_kb = ATmega1284["Program Memory Size(KB)"];
     int ATmega2560_kb = ATmega2560["Program Memory Size(KB)"];
 
-    int ATtiny3217_kb = ATtiny3217["Program Memory Size(KB)"];
-    int ATtiny1617_kb = ATtiny1617["Program Memory Size(KB)"];
-    int ATtiny817_kb = ATtiny817["Program Memory Size(KB)"];
+    //int ATtiny3217_kb = ATtiny3217["Program Memory Size(KB)"];
+    //int ATtiny1617_kb = ATtiny1617["Program Memory Size(KB)"];
+    //int ATtiny817_kb = ATtiny817["Program Memory Size(KB)"];
 
-    int ATmega168p_memory =  ATmega168p_kb * 1000;
+    //int ATmega168p_memory =  ATmega168p_kb * 1000;
     int ATmega328p_memory = ATmega328p_kb * 1000;
     int ATmega644p_memory = ATmega644p_kb * 1000;
     int ATmega1284_memory = ATmega1284_kb * 1000;
     int ATmega2560_memory = ATmega2560_kb * 1000;
 
-    int ATtiny3217_memory = ATtiny3217_kb * 1000;
-    int ATtiny1617_memory = ATtiny1617_kb * 1000;
-    int ATtiny817_memory = ATtiny817_kb * 1000;
+    //int ATtiny3217_memory = ATtiny3217_kb * 1000;
+    //int ATtiny1617_memory = ATtiny1617_kb * 1000;
+    //int ATtiny817_memory = ATtiny817_kb * 1000;
 
 
     /*std::cout << ATmega168p_memory << "\n";
@@ -68,14 +68,14 @@ int main(int argc, const char * argv[]) {
     int memory_array[8];
     int arraySize = 8;
 
-    memory_array[0] = ATmega168p_memory;
+    //memory_array[0] = ATmega168p_memory;
     memory_array[1] = ATmega328p_memory;
     memory_array[2] = ATmega644p_memory;
     memory_array[3] = ATmega1284_memory;
     memory_array[4] = ATmega2560_memory;
-    memory_array[5] = ATtiny3217_memory;
-    memory_array[6] = ATtiny1617_memory;
-    memory_array[7] = ATtiny817_memory;
+    //memory_array[5] = ATtiny3217_memory;
+    //memory_array[6] = ATtiny1617_memory;
+    //memory_array[7] = ATtiny817_memory;
 
       std::cout << "The memory sizes of each chip" << "\n";
 
@@ -88,7 +88,7 @@ int main(int argc, const char * argv[]) {
     std::cout << " "<< "\n";
 
 
-    std::cout << "The given memory is 31000 bytes, The smallest difference is : " << val << "\n";
+    std::cout << "The given memory is " << sketch_memory << " bytes, The smallest difference is : " << val << "\n";
    
  	
  	/*// hash map to match microchip name (key) and its memory(value)
@@ -165,14 +165,14 @@ void recommend(int size, int *arr){
 
 	// hash map to match microchip name (key) and its memory(value)
     std::map<std::string, int> m;
-    m["Atmega168p"] = arr[0];
+    //m["Atmega168p"] = arr[0];
     m["ATmega328p"] = arr[1];
     m["ATmega644p"] = arr[2];
     m["ATmega1284"] = arr[3];
     m["ATmega2560"] = arr[4];
-    m["ATtiny3217"] = arr[5];
-    m["ATtiny1617"] = arr[6];
-    m["ATtiny817"] = arr[7];
+    //m["ATtiny3217"] = arr[5];
+    //m["ATtiny1617"] = arr[6];
+    //m["ATtiny817"] = arr[7];
 
 	std::cout << " "<< "\n";
 
