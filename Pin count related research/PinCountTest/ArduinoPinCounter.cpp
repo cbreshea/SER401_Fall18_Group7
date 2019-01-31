5e5e3de616b7e99172e977833a5f5f28fb178cf4
ksbeasle <ksbeasle@asu.edu>
2019-01-31 02:42:54 -0700

    task #110
    Keyword search program successfully combined with ArduinoPinCounter.cpp program. Arduino sketch "simple1" added for future testing in pin counting.

--- Pin count related research/PinCountTest/ArduinoPinCounter.cpp
+++ Pin count related research/PinCountTest/ArduinoPinCounter.cpp
@@ -5,9 +5,21 @@ using namespace std;
 
 int main(){
 	string file;
+	string keyword = "dog";
+	string word;
+	int count = 0;
+	//cout << keyword;
 	cout << "Enter the file name + extension: \n";
 	cin >> file;
   ifstream myfile (file.c_str());
+  while(myfile >> word){
+		//cout << word << " ";
+		if(word == keyword){
+			count++;
+		}
+	}
+	cout << count;
+  /*
   if (myfile.is_open())
   {
     while ( getline (myfile,file) )
@@ -20,6 +32,6 @@ int main(){
   else cout << "Unable to open file"; 
 
   return 0;
-	
+	*/
 	
 }
