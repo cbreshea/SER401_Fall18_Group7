9af4206b5621364ae2890a69e75df7ecdce96470
scaneda1 <slcaneda@Stoievs-MacBook-Air.local>
2019-02-12 16:19:28 -0700

    fix hexToString

--- hexToString/hexToString.cpp
+++ hexToString/hexToString.cpp
@@ -14,13 +14,24 @@ void convertToBinary(std::string hexVal);
 int main(int argc, const char ** argv) {
 
 
-    const char *filename = "hello.txt";
+    /*const char *filename = "hello.txt";
     ifstream read_file(filename, fstream::in);
 
     std::string val;
     read_file >> convertToString(filename) >> val; // read hex value
-    cout << val; // display hex value
+    cout << val << "\n"; // display hex value*/
+
+    ifstream find("hello.txt");
+
+    string name;
+
+    while (find >> name)
+    {
+        //convertToString(name);
+    }
 	
+	convertToString(name);
+    
     std::string hexVal = "48656c6c6f";
 	convertToString(hexVal);
 	return 0;
@@ -30,19 +41,20 @@ int main(int argc, const char ** argv) {
 void convertToString(std::string hex){
 	int size = hex.length();
 	std::string word;
+	char * pEnd;
 	for(int i=0; i< size; i+=2)
 	{
 		//grab each byte
 	    string two = hex.substr(i,2);
-	    char single = (char) (int)strtol(two.c_str(), null, 16);
+	    char single = (char) (int)strtol(two.c_str(), &pEnd, 16);
 	    //shift to convert to string
 	    word.push_back(single);
 	}
-	cout << word;
+	cout << word << "\n";
 }
 
 void convertToBinary(std:: string hex){
-	string hex = "0xA";
+	hex = "0xA";
     stringstream str;
     str << hex << str;
     unsigned num;
