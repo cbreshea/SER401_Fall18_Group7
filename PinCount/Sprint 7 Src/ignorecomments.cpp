2316dddd9da134a474bcadd041cdf77cc0b9c5d6
Charles Breshears <cbreshea@asu.edu>
2019-02-26 08:59:12 -0700

    task 168 added an ignore comments cpp file

---
+++ PinCount/Sprint 7 Src/ignorecomments.cpp
@@ -0,0 +1,23 @@
+// *** PLEASE NOTE ***
+
+
+// THIS CODE IS MEANT TO PUSH THE GROUPS KNOWLEDGE FORWARD
+// I DO NOT TAKE CREDIT FOR WIRTING THIS CODE
+// I GOT THIS FROM https://stackoverflow.com/questions/13304028/c-reading-numbers-from-text-files-ignoring-comments
+// MORE COMPLETE CODE WILL COME IN THE COMING DAYS.
+
+
+while (std::getline(input_file, line))
+{
+    if (line[0] != "#" )
+    {
+        std::istringstream iss(line);
+        float num; // The number in the line
+
+        //while the iss is a number 
+        while ((iss >> num))
+        {
+            //look at the number
+        }
+    }
+}
