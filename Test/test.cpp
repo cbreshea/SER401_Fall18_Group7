ab12ebbc2655e2a2f1d46ceca8d3904aee78f776
Kahlil Beasley <ksbeasle@asu.edu>
2019-03-13 19:30:08 -0700

    Task #193
    Created a simple regex program to check if the adafruit library is being used.

---
+++ Test/test.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <regex>
+/*
+This example was derived from http://www.cplusplus.com/reference/regex/regex_search/
+*/
+int main ()
+{
+  std::string s ("#include <Adafruit_NeoPixel.h> ifdef __AVR__include <avr/power.h>#endif define PIN            6");
+  std::smatch m;
+  std::regex e ("\\b(Adafruit_NeoPixel.h)([^ ]*)");
+  while (std::regex_search (s,m,e)) {
+    for (auto x:m) std::cout << x << " ";
+    std::cout << std::endl;
+    s = m.suffix().str();
+  }
+
+  return 0;
+}
