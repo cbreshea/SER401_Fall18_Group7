a8796de20529b7c7e249309bca7ccba88a308de6
Kahlil Beasley <ksbeasle@asu.edu>
2019-03-15 16:16:19 -0700

    Task #199
    Modified the test cpp program to display the library and adjust the pin count.

--- Test/test.cpp
+++ Test/test.cpp
@@ -6,14 +6,17 @@ This example was derived from http://www.cplusplus.com/reference/regex/regex_sea
 */
 int main ()
 {
+    int pinCount = 0;
   std::string s ("#include <Adafruit_NeoPixel.h> ifdef __AVR__include <avr/power.h>#endif define PIN            6");
   std::smatch m;
   std::regex e ("\\b(Adafruit_NeoPixel.h)([^ ]*)");
-  while (std::regex_search (s,m,e)) {
-    for (auto x:m) std::cout << x << " ";
-    std::cout << std::endl;
+  if (std::regex_search (s,m,e)) {
+    for (auto x:m)
+        //std::cout << x << " ";
+    std::cout << "Neopixel library in use." << std::endl;
     s = m.suffix().str();
+      pinCount+=2;
   }
-
+    std::cout << pinCount << std::endl;
   return 0;
 }
