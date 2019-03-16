341ce82acb7a5622e4bda0d7e1de87a1859752dd
Charles Breshears <cbreshea@asu.edu>
2019-03-16 10:31:04 -0700

    task 190, added method to count number of occurances

--- PinCount/Multiple Line Comment ignore/multipleLine.cpp
+++ PinCount/Multiple Line Comment ignore/multipleLine.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -6,13 +7,25 @@ using namespace std;
 
 int main(void) {
     string line;
+    int count;
     ifstream file("cat.txt");
+    char ch[40] , c [40];
     if (file.is_open()){
         while(getline(file,line)){ // This will go line by line
             if(line.starts_with('/*')){
                 // it will go through and stop once there is a '*/'
+                if (line.starts_with('*/')){
+                    if (line.starts_with('*/'))continue;
+                }
+            }
+            // now count the number of instances the word occurs
+            while (file) {
+                file >> ch;
+                if(strcmp(ch, c)==0)
+                    count++;
+                <#statements#>
             }
         }
-        
+        cout << "Number of pins used is " << count << endl;
         file.close();
     }
