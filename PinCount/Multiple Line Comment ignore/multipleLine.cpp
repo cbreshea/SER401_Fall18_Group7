d565be0f972e5be513f6dde3153468d65ca65c0f
Kahlil Beasley <ksbeasle@asu.edu>
2019-03-19 23:37:58 -0700

    Task #207
    Progress on fixing the errors in the multi-line program.

--- PinCount/Multiple Line Comment ignore/multipleLine.cpp
+++ PinCount/Multiple Line Comment ignore/multipleLine.cpp
@@ -2,24 +2,25 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <string.h>
 
 using namespace std;
 
 int main(void) {
     string line;
     int count;
-    string word = "PinMode";
+    string word = "PIN";
     string randomBoard = "ATMEGA4609";
-    ifstream file("cat.txt");
+    ifstream file("simple.ino");
     char ch[40] , c [40];
     if (file.is_open()){
         while(getline(file,line)){ // This will go line by line
-            if(line.starts_with('/*')){
+            //if(line.starts_with('/*')){
                 // it will go through and stop once there is a '*/'
-                if (line.starts_with('*/')){
-                    if (line.starts_with('*/'))continue;
-                }
-            }
+                //if (line.starts_with('*/')){
+               //     if (line.starts_with('*/'))continue;
+             //   }
+           // }
             // now count the number of instances the word occurs
             while (file) {
                 file >> word;
@@ -33,3 +34,5 @@ int main(void) {
         file.close();
     }
 }
+
+
