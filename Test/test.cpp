7c6b16607c7db8b270d945ef996af30485ec6902
Kahlil Beasley <ksbeasle@asu.edu>
2019-03-22 19:30:16 -0700

    Task #213
    Tested accuracy.

--- Test/test.cpp
+++ Test/test.cpp
@@ -7,12 +7,12 @@ This example was derived from http://www.cplusplus.com/reference/regex/regex_sea
 int main ()
 {
     int pinCount = 0;
-  std::string s ("#include <Adafruit_NeoPixel.h> ifdef __AVR__include <avr/power.h>#endif define PIN            6");
+  std::string s ("<Adafruit_NeoPixel.h>");
   std::smatch m;
   std::regex e ("\\b(Adafruit_NeoPixel.h)([^ ]*)");
   if (std::regex_search (s,m,e)) {
     for (auto x:m)
-        //std::cout << x << " ";
+        std::cout << x << " ";
     std::cout << "Neopixel library in use." << std::endl;
   while (std::regex_search (s,m,e)) {
       for (auto x:m){
@@ -28,4 +28,5 @@ int main ()
   }
     std::cout << pinCount << std::endl;
   return 0;
+}
 }
