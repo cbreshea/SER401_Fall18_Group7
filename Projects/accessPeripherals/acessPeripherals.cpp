ba4d1064d10eb521a451fa3178db9cd87cdb8016
scaneda1 <slcaneda@Stoievs-MacBook-Air.local>
2019-03-27 23:04:53 -0700

    Task 225 complete

---
+++ Projects/accessPeripherals/acessPeripherals.cpp
@@ -0,0 +1,28 @@
+#include <sstream>
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <stdio.h>
+#include "json.hpp"
+#include <math.h>
+#include <limits>
+#include <string>
+using namespace std;
+
+int main(int argc, const char ** argv) {
+    
+    using json = nlohmann::json;
+    std::ifstream mega("MegaAVR.json");
+
+	  json megaAvr = json::parse(mega);
+
+    json temp = 0;//variable to hold pin count from microcontroller
+
+    for(int i = 0; i < megaAvr["MegaAVRs"].size(); ++i){
+      temp = megaAvr["MegaAVRs"][i]["Digital Communication Peripherals"];//this assignment is needed to convert to c++ int
+      cout << temp << endl;
+      
+    }
+
+    return 0;
+}
