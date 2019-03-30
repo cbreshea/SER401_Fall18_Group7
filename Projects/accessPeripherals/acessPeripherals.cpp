a1718e0c964db1a87acab46c9fb4a40409b2a297
scaneda1 <slcaneda@Stoievs-MacBook-Air.local>
2019-03-29 22:36:16 -0700

    task 227

--- Projects/accessPeripherals/acessPeripherals.cpp
+++ Projects/accessPeripherals/acessPeripherals.cpp
@@ -13,14 +13,24 @@ int main(int argc, const char ** argv) {
     
     using json = nlohmann::json;
     std::ifstream mega("MegaAVR.json");
+    std::ifstream tiny("TinyAVR.json");
 
 	  json megaAvr = json::parse(mega);
+	  json tinyAvr = json::parse(tiny);
 
-    json temp = 0;//variable to hold pin count from microcontroller
+    json temp1 = 0;//variable to hold peripheral info from microcontroller
+    json temp2 = 0;//variable to hold peripheral info from microcontroller
 
     for(int i = 0; i < megaAvr["MegaAVRs"].size(); ++i){
       temp = megaAvr["MegaAVRs"][i]["Digital Communication Peripherals"];//this assignment is needed to convert to c++ int
-      cout << temp << endl;
+      cout << temp1 << endl;
+      
+    }
+
+
+    for(int i = 0; i < tinyAvr["TinyAVRs"].size(); ++i){
+      temp = tinyAvr["TinyAVRs"][i]["Digital Communication Peripherals"];//this assignment is needed to convert to c++ int
+      cout << temp2 << endl;
       
     }
 
