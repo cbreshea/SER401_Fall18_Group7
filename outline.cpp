50b5260e595a265456f7d5236632dc114382ac41
Charles Breshears <cbreshea@asu.edu>
2019-03-30 20:24:34 -0700

    task 219

--- outline.cpp
+++ outline.cpp
@@ -9,3 +9,11 @@ main
 	- *ACTUAL PROGARM OF PINCOUNT/MEMORY MERGED INTO ONE* ()
 	- readme
 
+
+	// We also need to get a copy of the entire directory so it will be like this:
+
+	main 
+
+	Everything else 
+	// entire github directory 
+
