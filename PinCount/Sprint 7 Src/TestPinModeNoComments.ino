555a10e8a4de5d534c1b28863cf8cc059e2bbb25
Charles Breshears <cbreshea@asu.edu>
2019-04-06 06:51:20 -0700

    Task 234 finished

--- PinCount/Sprint 7 Src/TestPinModeNoComments.ino
+++ PinCount/Sprint 7 Src/TestPinModeNoComments.ino
@@ -1,6 +1,11 @@
 // We are now going to disreguard this .ino file as it no longer serves purpose
 
 
+void main(){
+	setup();
+	Serail.print("This is the end of the file");
+}
+
 void setup(){
 
 pinMode = (11,HIGH)
